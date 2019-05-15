b026ccb1aa64b9ca76051eb26df6cb2ef08edfa4
waterlilywater <kawitakandpal@gmail.com>
2019-05-15 13:42:53 -0400

    fifth commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Route, Link} from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
-
+import Album from './components/Album';
 
 class App extends Component {
    render() {
@@ -12,13 +12,14 @@ class App extends Component {
          <header>
          <nav>
              <Link to='/'>Landing</Link>
-             <Link to='/library'>Library</Link>
+             <Link to='/library'>Library</Link>//a tag
          </nav>
            <h1>Bloc Jams</h1>
          </header>
          <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
+          <Route path="/album" component={Album}/>//this is the path and this is the view that goes with the view, not visible
          </main>
        </div>
      );
