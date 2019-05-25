88fa08b76d9e6449ac2b4bb58648df1ffb5a0e66
waterlilywater <kawitakandpal@gmail.com>
2019-05-24 20:02:21 -0400

    refactored Album.js

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,13 +1,99 @@
 import React, { Component } from 'react';
 
+import albumData from './../data/albums';
+
+
+
 class Album extends Component {
-  render() {
-    return (
-      <section className="album">
-        {this.props.match.params.slug} Album will go here
-      </section>
-    );
+
+  constructor(props){
+
+    super(props);
+
+
+
+    const album = albumData.find(album =>{
+
+        return album.slug === this.props.match.params.slug
+
+    });
+
+
+
+    this.state ={
+
+        album: album,
+
+    }
+
   }
+
+  render() {
+
+    return (
+
+      <section className="album">
+
+        <section id="album-info">
+
+           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+
+           <div className="album-details">
+
+             <h1 id="album-title">{this.state.album.title}</h1>
+
+             <h2 className="artist">{this.state.album.artist}</h2>
+
+             <div id="release-info">{this.state.album.releaseInfo}</div>
+
+           </div>
+
+         </section>
+
+
+
+      <table id="song-list">
+
+          <tbody>
+
+            {this.state.album.songs.map( (song, index) =>
+
+              <tr key={index} >
+
+                <td className="song-actions">
+
+                  <button>
+
+                    <span className="song-number">{index+1}</span>
+
+                    <span className="ion-play"></span>
+
+                    <span className="ion-pause"></span>
+
+                  </button>
+
+                </td>
+
+                <td className="song-title">{song.title}</td>
+
+                <td className="song-duration">{song.duration}</td>
+
+              </tr>
+
+            )}
+
+          </tbody>
+
+        </table>
+
+        </section>
+
+    );
+
+  }
+
 }
+
+
 
 export default Album;
