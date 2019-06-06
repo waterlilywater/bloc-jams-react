36f284d78433eaaeb0d93c8ae72dda4f6df8ccdd
waterlilywater <kawitakandpal@gmail.com>
2019-06-06 16:25:37 -0400

    player bar range inputs

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,100 +2,86 @@ import React, { Component } from 'react';
 import albumData from './../data/albums';
 import PlayerBar from './PlayerBar';
 
-
-
 class Album extends Component {
-
-  constructor(props){
-
+  constructor(props) {
     super(props);
-
-
-
-    const album = albumData.find(album =>{
-
-        return album.slug === this.props.match.params.slug
-
+    const album = albumData.find(album => {
+      return album.slug === this.props.match.params.slug
     });
-
-
-    this.state ={
-
-        album: album,
-        currentSong: album.songs[0],
-        isPlaying: false,
-        currentTime: 0,
-        duration: album.songs[0].duration,
-        hover: false
+    this.state = {
+      album: album,
+      currentSong: album.songs[0],
+      currentTime: 0,
+      duration: album.songs[0].duration,
+      isPlaying: false,
+      hover: false,
+      currentVolume: 0.8
     };
 
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
 
-
-  this.audioElement = document.createElement('audio');
-  this.audioElement.src = album.songs[0].audioSrc;
-
+  formatTime(seconds) {
+var mins = parseInt(seconds/60);
+var secs = parseInt(seconds % 60);
+return mins + ":" + secs;
 }
 
-componentDidMount() {
-  this.eventListeners = {
-    timeupdate: e => {
-      this.setState({ currentTime: this.audioElement.currentTime });
-    },
-    durationchange: e => {
-      this.setState({ duration: this.audioElement.duration });
-    }
-  };
-  this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
-  this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
-}
-
-componentWillUnmount() {
-     this.audioElement.src = null;
-     this.audioElement = null;
+  componentDidMount() {
+     this.eventListeners = {
+       timeupdate: e => {
+         this.setState({ currentTime: this.audioElement.currentTime });
+       },
+       durationchange: e => {
+         this.setState({ duration: this.audioElement.duration });
+       }
+     };
+     this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+     this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
    }
 
-play() {
-  this.audioElement.play();
-  this.setState({ isPlaying: true });
-}
+   componentWillUnmount() {
+     this.audioElement.src = null; // setting the src to null means the song isn't playing anymore
+     this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+     this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+   }
 
-pause() {
-  this.audioElement.pause();
-  this.setState({ isPlaying: false});
-}
-
-setSong(song) {
-  this.audioElement.src = song.audioSrc;
-  this.setState({ currentSong: song });
-}
-
-
-handleSongClick(song) {
-  const isSameSong = this.state.currentSong === song;
-
-
-if (this.state.isPlaying && isSameSong) {
-  this.pause();
-
-
-} else {
-  if (!isSameSong) {this.setSong(song); }
-  this.play();
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
   }
-}
 
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
 
-hoverOn (song) {
-     this.setState ({hover: song});
-}
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
 
-hoverOff () {
-    this.setState ({hover: null});
-}
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
+  }
 
+  hoverOn(song) {
+    this.setState({ hover: song });
+  }
 
-displayIcon (song, i) {
-  const isSameSong = this.state.currentSong === song;
+  hoverOff() {
+    this.setState({ hover: null });
+  }
+
+  displayIcon(song, i) {
+    const isSameSong = this.state.currentSong === song;
     if ((this.state.hover === song && !isSameSong) || (!this.state.isPlaying && isSameSong)) {
       return <span className='ion-md-play'></span>
     } else if (this.state.isPlaying && isSameSong) {
@@ -107,19 +93,20 @@ displayIcon (song, i) {
 
   handlePrevClick() {
     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
-      const newIndex = Math.max(0, currentIndex - 1);
-      const newSong = this.state.album.songs[newIndex];
-      this.setSong(newSong);
-      this.play();
-      }
-
-  handleNextClick(){
-    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
-      const newIndex = Math.max(0, currentIndex + 1);
-      const newSong = this.state.album.songs[newIndex];
-      this.setSong(newSong);
-      this.play();
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
   }
+
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(this.state.album.songs.length - 1, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
 
   handleTimeChange(e) {
      const newTime = this.audioElement.duration * e.target.value;
@@ -127,86 +114,60 @@ displayIcon (song, i) {
      this.setState({ currentTime: newTime });
    }
 
-  render() {
 
-    return (
-
-
-      <section className="album">
-
-        <section id="album-info">
-
-           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-
-           <div className="album-details">
-
-             <h1 id="album-title">{this.state.album.title}</h1>
-
-             <h2 className="artist">{this.state.album.artist}</h2>
-
-             <div id="release-info">{this.state.album.releaseInfo}</div>
-
-           </div>
-
-         </section>
-
-      <table id="song-list">
-
-        <colgroup>
-
-          <col id="song-number-column" />
-          <col id="song-title-column" />
-          <col id="song-duration-column" />
-
-        </colgroup>
-
-
-          <tbody>
-
-            {this.state.album.songs.map( (song, index) =>
-
-              <tr className="song" key={index} onClick={() => this.handleSongClick(song)} onMouseEnter={() => this.hoverOn(song)}
-              onMouseLeave={() => this.hoverOff()}>
-
-                <td className="song-actions">
-
-                  <button>
-
-                    {this.displayIcon(song, index)}
-
-                  </button>
-
-                </td>
-
-
-                <td className="song-title">{song.title}</td>
-
-                <td className="song-duration">{song.duration}</td>
-
-              </tr>
-
-            )}
-
-          </tbody>
-
-        </table>
-        <PlayerBar
-         isPlaying={this.state.isPlaying}
-         currentSong={this.state.currentSong}
-         currentTime={this.audioElement.currentTime}
-         duration={this.audioElement.duration}
-         handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-         handlePrevClick={() => this.handlePrevClick()}
-         handleNextClick={() => this.handleNextClick()}
-         handleTimeChange={(e) => this.handleTimeChange(e)}
-       />
-        </section>
-
-    );
+  handleVolumeChange(e) {
+    const newVolume = e.target.value;
+    this.audioElement.volume = newVolume;
+    this.setState({ currentVolume: newVolume })
 
   }
 
+  render() {
+    return (
+      <section className="album">
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody>
+            {this.state.album.songs.map((song, index) =>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)} onMouseEnter={() => this.hoverOn(song)}
+                onMouseLeave={() => this.hoverOff()}>
+                <td className="song-actions">
+                  <button>
+                    {this.displayIcon(song, index)}
+                  </button>
+                </td>
+                <td className="song-title">{song.title}</td>
+                <td className="song-duration">{this.formatTime(parseFloat(song.duration))}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+        <PlayerBar
+          isPlaying={this.state.isPlaying}
+          currentSong={this.state.currentSong}
+          currentTime={this.formatTime(this.audioElement.currentTime)}
+          duration={this.formatTime(this.audioElement.duration)}
+          currentVolume={this.state.currentVolume}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
+          handleTimeChange={(e) => this.handleTimeChange(e)}
+          handleVolumeChange={(e) => this.handleVolumeChange(e)}
+        />
+      </section>
+    );
+  }
 }
-
-
 export default Album;
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -1,42 +1,47 @@
+
+
 import React, { Component } from 'react';
 
  class PlayerBar extends Component {
    render() {
      return (
        <section className="player-bar">
-       <section id="buttons">
-
-        <button id="previous" onClick={this.props.handlePrevClick}>
-           <span className="ion-skip-backward"></span>
-         </button>
-
-        <button id="play-pause" onClick={this.props.handleSongClick} >
-         <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
-         </button>
-
-        <button id="next" onClick={this.props.handleNextClick}>
-           <span className="ion-skip-forward"></span>
-        </button>
-      </section>
-
-       <section id="time-control">
-       <div className="current-time">{this.props.currentTime}</div>
-         <input
-           type="range"
-           className="seek-bar"
-           value={(this.props.currentTime / this.props.duration) || 0}
-           max="1"
-           min="0"
-           step="0.01"
-           onChange={this.props.handleTimeChange}
-         />
-         <div className="total-time">{this.props.duration}</div>
-       </section>
-       <section id="volume-control">
-         <div className="icon ion-volume-low"></div>
-         <input type="range" className="seek-bar" value="80" />
-         <div className="icon ion-volume-high"></div>
-       </section>
+         <section id="buttons">
+           <button id="previous" onClick={this.props.handlePrevClick}>
+             <ion-icon className="ion-skip-backward"></ion-icon>
+           </button>
+           <button id="play-pause" onClick={this.props.handleSongClick} >
+             <ion-icon name={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></ion-icon>
+           </button>
+           <button id="next" onClick={this.props.handleNextClick}>
+             <ion-icon className="ion-skip-forward"></ion-icon>
+           </button>
+         </section>
+         <section id="time-control">
+           <div className="current-time">{this.props.currentTime}</div>
+           <input
+             type="range"
+             className="seek-bar"
+             value={(this.props.currentTime / this.props.duration) || 0}
+             max="1"
+             min="0"
+             step="0.01"
+             onChange={this.props.handleTimeChange}
+           />
+           <div className="total-time">{this.props.duration}</div>
+         </section>
+         <section id="volume-control">
+           <div className="icon ion-volume-low"></div>
+           <input
+            type="range"
+            className="seek-bar"
+            value={this.props.currentVolume}
+            max="1"
+            min="0"
+            step="0.01"
+            onChange={this.props.handleVolumeChange} />
+           <div className="icon ion-volume-high"></div>
+         </section>
        </section>
      );
    }
